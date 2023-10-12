3003376d4f354c9a582290e189bdf903dafc0c41
amitmungare <amungare27@gmail.com>
2023-10-12 15:53:52 +0530

    complete sign in

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -38,7 +38,7 @@ export default function SignIn() {
       }
       setLoading(false);
       setError(null);
-      navigate('/sign-in');
+      navigate('/');
     } catch (error) {
       setLoading(false);
       setError(error.message)
@@ -49,21 +49,20 @@ export default function SignIn() {
   return (
     <div className='p-3 max-w-lg mx-auto'>
 
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
 
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handelChange}/>
         <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handelChange}/>
         <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handelChange}/>
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {loading ? 'Loading...' : 'Sign Up'}
+          {loading ? 'Loading...' : 'Sign In'}
         </button>
       </form>
 
       <div className='flex gap-2 mt-5'>
-        <p>Have an account?</p>
-        <Link to={"/sign-in"}>
-          <span className='text-blue-700'>Sign in</span>
+        <p>Do not have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
       {error && <p className='text-red-500 mt-5'>{error}</p>}
