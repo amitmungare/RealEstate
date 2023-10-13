04c09628f651d2fda63a0ada552783fe2c5e4480
amitmungare <amungare27@gmail.com>
2023-10-13 22:16:13 +0530

    added OAuth

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDsXOI1hZf-c_w3Gq5hQ5SEvdURYbr83oE',
+  authDomain: "real-estate-b7cb0.firebaseapp.com",
+  projectId: "real-estate-b7cb0",
+  storageBucket: "real-estate-b7cb0.appspot.com",
+  messagingSenderId: "17130422293",
+  appId: "1:17130422293:web:c18b16efbab3adae298e1c"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
